339bfdf2e70d0443892c345baee8099f6fbcec68
yongbin <liyobi@gmail.com>
2018-06-29 10:51:37 +0900

    gson expore add

--- src/main/java/com/hello/api/ApiResultData.java
+++ src/main/java/com/hello/api/ApiResultData.java
@@ -3,11 +3,13 @@ package com.hello.api;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.http.HttpStatus;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+//import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlCData;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import com.google.gson.annotations.Expose;
+
 import lombok.Data;
 
 
@@ -15,23 +17,27 @@ import lombok.Data;
 @JacksonXmlRootElement(namespace="com.hello" , localName="response")
 public class ApiResultData {
     
-    @JsonIgnore
+    //@JsonIgnore
     private HttpStatus status;
     
     @JacksonXmlProperty
+    @Expose
     private String message;
     
     @JacksonXmlProperty
+    @Expose
     private int code;
     
     @JacksonXmlElementWrapper(localName = "errors")
     @JacksonXmlCData
     @JacksonXmlProperty(localName="error" )
+    @Expose
     private List<String> errors;
     
     
     @JacksonXmlCData
     @JacksonXmlProperty(localName="datas" )
+    @Expose
     private List<?> datas;
     
     public ApiResultData(HttpStatus status) {
